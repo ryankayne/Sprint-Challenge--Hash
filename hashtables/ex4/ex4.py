004ff94e33f0f63817244a445c6013a2f7a58123
ryankayne <coope1rk@gmail.com>
2020-05-13 23:59:49 -0400

    mvp for 2, 4, and 5

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,38 @@
+# b = [ 1, -1, 2, 3, -4, -3, 4, -5, 6, 7 ]
+
 def has_negatives(a):
+    # neg = {}
+    # pos = {}
+    # result = []
 
-    """
-    YOUR CODE HERE
-    """
+    # for x in a:
+    #     if x < 0:
+    #         neg[x] = abs(x)
+    #     elif x > 0:
+    #         pos[x] = x
+    # for x in neg:
+    #     if neg[x] == pos[x]:
+    #         result.append(neg[x]) 
+    #     # (neg.get(abs(x)) + x) == 0:
+    #     #         result.append(abs(x))
+    #     #     else:
+    #     #         neg[abs(x)] = x
+    # print(neg)
+    # print(pos)        
 
+    d = {}
+    result = []
+
+    for x in a:
+        d[x] = abs(x)
+        if d[x] in range(len(a)):
+            if x < 0:
+                result.append(abs(x))
     return result
+# for every item x in array a, I want to store x in a dictionary. If the dictionary already has the key/value, then append to results list.
 
+
+# print(has_negatives(b))
 
 if __name__ == "__main__":
     print(has_negatives([-1,-2,1,2,3,4,-4]))
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,10 +1,38 @@
 def finder(files, queries):
+    d = {}
+    result = []
 
-    """
-    YOUR CODE HERE
-    """
-
+    for x in files:
+        text = x.split("/")
+        if text[-1] not in d:
+            # d[text[-1]] = x
+            d[text[-1]] = [x]
+        else:
+            # print("Not important")
+            d[text[-1]].append(x)
+            # return None
+    for x in queries:
+        if x in d:
+            # for y in len(d[x]):
+            for y in range(len(d[x])):
+                # access length so you can always access the first value at index 0. For y in range of d[x], which would be "foo: [path]", [path] being index 0.
+                result.append(d[x][y])
+                # appending the full path value of the current key to the results list!
     return result
+
+# w3
+# txt = "/usr/local/share/foo.txt"
+# x = txt.split("/")
+# print(x)
+
+# https://stackoverflow.com/questions/1950672/python-split-list-into-list-of-dicts
+# result = [{}]
+# for item in data:
+#     key, val = item.split(":", 1)
+#     if key in result[-1]:
+#         result.append({})
+#     result[-1][key] = val
+
 
 
 if __name__ == "__main__":
